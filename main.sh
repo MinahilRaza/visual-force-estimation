aa2f8d1a3de26f962d041b857d59fe0e79e674de
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-03-05 08:14:02 -0800

    updated main.sh

--- main.sh
+++ main.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 
-python train.py --batch_size 8 --lr 0.00001 --num_epochs 50 --force_runs 1 2 3 4 6 8 --no_force_runs 1 4
+python train.py --batch_size 16 --lr 0.0001 --num_epochs 50 --force_runs 1 2 3 4 6 8 --no_force_runs 1 4 --lr_scheduler
