83369a1f78e49f4221c9458c53ad216be7fd0df0
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-03-31 17:17:37 -0700

    option to run training for auto encoder or end to end model

--- main.sh
+++ main.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/bash
 
-# python train.py --batch_size 8 --lr 0.00001 --model res_net --num_epochs 50 --force_runs 1 2 3 4 6 8 --no_force_runs 1 4
+TRAIN_AUTOENCODER=true
+TRAIN_FORCE_ESTIMATION=false
 
-python train.py --batch_size 8 --lr 0.0005 --model efficientnet_v2_m --num_epochs 30 --force_runs 1 2 3 4 6 8 --no_force_runs 1 4
+if $TRAIN_FORCE_ESTIMATION; then
+    python train.py --batch_size 8 --lr 0.00001 --model res_net --num_epochs 50 --force_runs 1 2 --no_force_runs 1 4
+fi
+
+if $TRAIN_AUTOENCODER; then
+    python train_auto_encoder.py --batch_size 4 --num_epochs 30 --lr 0.0001
+fi
+
+# python train.py --batch_size 8 --lr 0.0005 --model efficientnet_v2_m --num_epochs 30 --force_runs 1 2 3 4 6 8 --no_force_runs 1 4
