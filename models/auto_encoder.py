17ff57e2233f77618a9d4c205191db4f244841da
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-04-03 15:40:07 -0700

    added depth wise separable convolutions to decoder model

--- models/auto_encoder.py
+++ models/auto_encoder.py
@@ -3,6 +3,20 @@ import torch.nn as nn
 import torchvision.models as models
 
 
+class DepthwiseSeparableConv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super().__init__()
+        self.depthwise = nn.Conv2d(in_channels, in_channels, kernel_size=kernel_size,
+                                   stride=stride, padding=padding, groups=in_channels)
+        self.pointwise = nn.Conv2d(
+            in_channels, out_channels, kernel_size=1, stride=1)
+
+    def forward(self, x):
+        x = self.depthwise(x)
+        x = self.pointwise(x)
+        return x
+
+
 class UpSampleBlock(nn.Module):
     def __init__(self, in_channels, out_channels, upsample_scale=2):
         super().__init__()
@@ -12,8 +26,8 @@ class UpSampleBlock(nn.Module):
             nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
-        self.conv1 = nn.Conv2d(out_channels, out_channels,
-                               kernel_size=3, stride=1, padding=1)
+        self.conv1 = DepthwiseSeparableConv(out_channels, out_channels,
+                                            kernel_size=3, stride=1, padding=1)
         self.bn1 = nn.BatchNorm2d(out_channels)
         self.relu = nn.ReLU(inplace=True)
 
