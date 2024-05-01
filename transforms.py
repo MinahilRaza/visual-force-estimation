722d493d7a85564f9065915e6e258b05409115b4
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-05-01 09:18:03 -0700

    fixed bug in custom crop module for testing

--- transforms.py
+++ transforms.py
@@ -31,9 +31,10 @@ class CropBottom(torch.nn.Module):
         """
         crop_height, crop_width = self.output_size
         img_width, img_height = F.get_image_size(img)
+        left = max(0, (img_width - crop_width) // 2)
         top = max(0, img_height - crop_height)
 
-        return F.crop(img, top, 0, crop_height, crop_width)
+        return F.crop(img, top, left, crop_height, crop_width)
 
     def __repr__(self):
         return self.__class__.__name__ + '(output_size={0})'.format(self.output_size)
@@ -55,3 +56,14 @@ if __name__ == "__main__":
         cropped_image_np.reshape(-1, cropped_image_np.shape[2]), axis=0)
     assert np.any(np.all(unique_colors == [0, 0, 255], axis=1))
     assert len(unique_colors) == 1
+
+    image_path = "data/images/dec6_force_no_TA_lastP_randomPosHeight_cs100_run1_left/zed_left_1.png"
+
+    # Load an example image and display it:
+    image = Image.open(image_path)
+    image_tensor = F.to_tensor(image)
+
+    crop_module = CropBottom((224, 224))
+    cropped_image_tensor = crop_module(image_tensor)
+    cropped_image = F.to_pil_image(cropped_image_tensor)
+    cropped_image.show()
