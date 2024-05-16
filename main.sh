a9062894eefbe278fe876d38303a3301146d97ce
Tim Reinhart <tim.reinhart@bluewin.ch>
2024-05-15 17:58:34 -0700

    updated run script for transformer models

--- main.sh
+++ main.sh
@@ -2,10 +2,10 @@
 
 TRAIN_AUTOENCODER=false
 TRAIN_FORCE_ESTIMATION=true
+USE_TRANSFORMER=false
 
 vae_model_path="weights/auto_encoder/resnet50/"
-# fe_model_path="weights/force_estimation_network/overfitting_2/resnet50"
-fe_model_path="weights/force_estimation_network/resnet50"
+fe_model_path="weights/force_estimation_network"
 fe_model_state=robot
 
 if $TRAIN_AUTOENCODER; then
@@ -17,16 +17,37 @@ if $TRAIN_AUTOENCODER; then
 fi
 
 if $TRAIN_FORCE_ESTIMATION; then
-    python train.py --batch_size 32 \
-        --lr 0.00001 \
-        --model res_net \
-        --num_epochs 200 \
-        --force_runs 1 \
-        --out_dir $fe_model_path \
-        --normalize_targets \
-        --use_acceleration \
-        --state $fe_model_state \
-        --overfit
+    if $USE_TRANSFORMER; then
+        transformer_model_path="${fe_model_path}/transformer"
 
-    python evaluate.py --run 1 --weights $fe_model_path --model res_net --use_acceleration --overfit --state $fe_model_state
+        python train_transformer.py --batch_size 32 \
+            --lr 0.00002 \
+            --num_epochs 1 \
+            --force_runs 1 \
+            --out_dir $transformer_model_path \
+            --normalize_targets \
+            --use_acceleration \
+            --seq_length 10 \
+            --overfit
+
+        python evaluate.py --run 9 --weights $transformer_model_path --model res_net --model_type transformer --use_acceleration --overfit --state $fe_model_state
+        python evaluate.py --run 10 --weights $transformer_model_path --model res_net --model_type transformer --use_acceleration --overfit --state $fe_model_state
+        python evaluate.py --run 11 --weights $transformer_model_path --model res_net --model_type transformer --use_acceleration --overfit --state $fe_model_state
+    else
+        normal_model_path="${fe_model_path}/feed_forward"
+
+        python train.py --batch_size 32 \
+            --lr 0.00002 \
+            --model res_net \
+            --num_epochs 1 \
+            --force_runs 1 \
+            --out_dir $normal_model_path \
+            --normalize_targets \
+            --use_acceleration \
+            --state $fe_model_state
+
+        python evaluate.py --run 9 --weights $normal_model_path --model res_net --model_type vision_robot --use_acceleration --overfit --state $fe_model_state
+        python evaluate.py --run 10 --weights $normal_model_path --model res_net --model_type vision_robot --use_acceleration --overfit --state $fe_model_state
+        python evaluate.py --run 11 --weights $normal_model_path --model res_net --model_type vision_robot --use_acceleration --overfit --state $fe_model_state
+    fi
 fi
